bea488c3128daeccddf20adb106edfed44c67fbe
Neo <184576226@qq.com>
2016-06-07 17:56:26 +0800

    blog

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../../favicon.ico">
+    
+  <title>Laravel</title>
+
+  <!-- Fonts -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+  <link href="//libs.baidu.com/fontawesome/4.0.3/css/font-awesome.min.css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+
+  <!-- Bootstrap core CSS -->
+  <link href="{{ asset('plugins/bootstrap/bootstrap-4.0.0-alpha/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link rel="stylesheet" href="{{ asset('plugins/bootstrap/bootstrap-4.0.0-alpha/docs/examples/blog/blog.css') }}">
+
+  <style>
+  body {
+	font-family: 'Lato';
+  }
+
+  .fa-btn {
+	margin-right: 6px;
+  }
+  </style>
+</head>
+<body>
+	<!-- #section:basics/banner.layout -->
+	<!-- /section:basics/banner.layout -->
+
+	<!-- #section:blog/blog-masthead.layout -->
+	@include('layouts.blog.blog-masthead')
+	<!-- /section:blog/blog-masthead.layout -->
+
+	<!-- #section:blog/blog-header.layout -->
+	@include('layouts.blog.blog-header')
+	<!-- /section:blog/blog-header.layout -->
+
+	<div class="container">
+
+		<div class="row">
+
+			<div class="col-sm-8 blog-main">@yield('content')</div>
+			<!-- /.blog-main -->
+
+			<div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+				<!-- #section:blog/blog-sidebar.layout -->
+				@include('layouts.blog.blog-sidebar')
+				<!-- /section:blog/blog-sidebar.layout -->
+			</div>
+			<!-- /.blog-sidebar -->
+
+		</div>
+		<!-- /.row -->
+
+	</div>
+	<!-- /.container -->
+
+	<!-- #section:blog/blog-footer.layout -->
+	@include('layouts.blog.blog-footer')
+	<!-- /section:blog/blog-footer.layout -->
+
+	<!-- JavaScripts -->
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="{{ asset('plugins/bootstrap/bootstrap-4.0.0-alpha/dist/js/bootstrap.min.js') }}"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="http://v3.bootcss.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
---
+++ resources/views/layouts/basics/banner.blade.php
@@ -0,0 +1,19 @@
+<div class="banner">
+	<ul>
+		<li>
+			<div class="inner">
+				<p>This is a slide.</p>
+			</div>
+		</li>
+		<li>
+			<div class="inner">
+				<p>This is another slide.</p>
+			</div>
+		</li>
+		<li>
+			<div class="inner">
+				<p>This is a final slide.</p>
+			</div>
+		</li>
+	</ul>
+</div>
