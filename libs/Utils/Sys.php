df4b6973ec72df25c4c10f9e2576c6584de41c34
Neo <184576226@qq.com>
2016-06-17 15:43:01 +0800

    route

--- libs/Utils/Sys.php
+++ libs/Utils/Sys.php
@@ -6,7 +6,7 @@ class Sys
     public static function action($action, $options = [])
     {
         list($a,$c) = explode('.', $action);
-        return ['uses'=>'\\Modules\\'.$a.'\\Controller\\'.$c ,'as'=>isset($options['as'])?$options['as']:'', 'prefix'=>isset($options['prefix'])?$options['prefix']:'', 'relate'=>isset($options['prefix'])?$options['prefix']:''];
+        return ['uses'=>'\\Modules\\'.$a.'\\Controller\\'.$c ,'as'=>isset($options['as'])?$options['as']:'', 'prefix'=>isset($options['prefix'])?$options['prefix']:'', 'relate'=>isset($options['relate'])?$options['relate']:''];
     }
     
 }
