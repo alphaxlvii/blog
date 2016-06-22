102a806372d65a88d7678a3ba6b057ae09fbe384
Neo <184576226@qq.com>
2016-06-22 17:14:06 +0800

    navbar

--- modules/Blog/Controller/NavbarController.php
+++ modules/Blog/Controller/NavbarController.php
@@ -10,6 +10,13 @@ class NavbarController extends Controller
 {
     public static function Navbar()
     {
-        return ArrayUtils::obj2Arr(Navbar::select('id','navbar','title')->orderBy('sort','asc')->get());
+        $nav_can_enter = config('blog.nav_can_enter');
+        $nav_can_enter_arr = empty($nav_can_enter) ? null : explode('|', $nav_can_enter);
+        $navbars = Navbar::select('id','navbar','title')->orderBy('sort','asc');
+        if(!is_null($navbars)){
+            $navbars = $navbars->whereIn('navbar',$nav_can_enter_arr);
+        }
+        $navbars = $navbars->get();
+        return ArrayUtils::obj2Arr($navbars);
     }
 }
